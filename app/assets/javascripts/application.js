44d824314e186b866b670cc59039427206617c6c
Lia Skalkos <hello@liaskalkos.net>
2015-07-28 17:29:19 -0400

    Experiment with Highcharts.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require highcharts
+//= require highcharts-3d
 //= require turbolinks
 //= require_tree .
--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,8 +1,17 @@
+
 class StarsController < ApplicationController
 
   def index
     @stars = Star.all_by_apparent_magnitude(6.0)
+    byebug
+    @star_coordinates = @stars.map{|star| star.position.to_a}
+
+
+    respond_to do |f|
+      f.js
+    end
   end
+
 
 
 end
