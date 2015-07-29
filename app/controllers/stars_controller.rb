08012a20c2621189d072a58cc9c3e417f284e28d
Lia Skalkos <hello@liaskalkos.net>
2015-07-29 11:18:51 -0400

    Highchart JS working placehold

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -3,13 +3,12 @@ class StarsController < ApplicationController
 
   def index
     @stars = Star.all_by_apparent_magnitude(6.0)
-    byebug
     @star_coordinates = @stars.map{|star| star.position.to_a}
 
-
-    respond_to do |f|
-      f.js
+    respond_to do |format|
+      format.js
     end
+
   end
 
 
