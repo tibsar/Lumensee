94156eddeccd7a98439b6137bb193a1695b094c8
tibsar <sxt4866@rit.edu>
2015-07-29 11:32:31 -0400

    adds actions to the controlleR

--- app/controllers/observers_controller.rb
+++ app/controllers/observers_controller.rb
@@ -2,6 +2,9 @@ class ObserversController < ApplicationController
   def create
     @latitude = params["coords"]["latitude"].to_f
     @longitude = params["coords"]["longitude"].to_f
+    @stars = []
+    obs = Observer.new(@latitude, @longitude, @stars)
+    binding.pry
     #@pollution = params["pollution"].to_f
   end 
 end 
