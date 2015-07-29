7dcff30cec4bc6b918266772a2a67a6aa8b7b4e5
Lia Skalkos <hello@liaskalkos.net>
2015-07-29 15:38:37 -0400

    Integrates Star model and chart response into observers_controller.rb

--- app/controllers/observers_controller.rb
+++ app/controllers/observers_controller.rb
@@ -1,11 +1,31 @@
 class ObserversController < ApplicationController 
   def create
-    binding.pry
     @latitude = params["coords"]["latitude"].to_f
     @longitude = params["coords"]["longitude"].to_f
     @pollution = params["pollution"].to_f
     @stars = Star.all_by_apparent_magnitude(@pollution)
     obs = Observer.new(@latitude, @longitude, @stars)
     can_see = obs.starsVisible
-  end 
+
+    @star_points = can_see.map do |star|
+
+      radius = 4*(1.25**(6.5-star.appmag)).round(2)
+
+      {
+          marker: {radius: radius},
+          x: star.position.x,
+          y: star.position.y,
+          z: star.position.z
+      }
+
+    end
+
+    @star_points.shift
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+
 end 
