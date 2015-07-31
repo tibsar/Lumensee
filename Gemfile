960e48be51c612758d8ba7a2c10f4c469050285b
Lia Skalkos <hello@liaskalkos.net>
2015-07-31 09:33:26 -0400

    Adds in x-axis standard deviation buttonto interface

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'responders'
 
 gem 'pg'
 
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/observers_controller.rb
+++ app/controllers/observers_controller.rb
@@ -13,6 +13,12 @@ class ObserversController < ApplicationController
     @z_array = obs.stars.collect{|s| s.position.z}
     @average_z = @z_array.inject{ |sum, el| sum + el }.to_f / @z_array.size
 
+
+    @x_mean = (@x_array.inject(:+))/(@x_array.length.to_f)
+    @x_sum_of_deviations = @x_array.inject(0){|accum, i| accum + (i - @x_mean) ** 2 }
+    @x_standard_deviation = Math.sqrt(@x_sum_of_deviations/(@x_array.length-1).to_f)
+
+
     @stars = obs.plot_visible_stars
     respond_to do |format|
       format.js
